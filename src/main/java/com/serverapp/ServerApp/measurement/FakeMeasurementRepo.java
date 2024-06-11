41f040beb93c179e735fac3294ff2cfa4bcc23fc
Ludovico Passari <ludovicopassari9@gmail.com>
2024-06-11 20:53:38 +0200

    app funzionante al minimo dei requisiti

--- src/main/java/com/serverapp/ServerApp/measurement/FakeMeasurementRepo.java
+++
@@ -1,6 +0,0 @@
-package com.serverapp.ServerApp.measurement;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FakeMeasurementRepo extends JpaRepository<FakeMeasurement, Long> {
-}
